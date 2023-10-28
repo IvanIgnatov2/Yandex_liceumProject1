bd9583849fe004eba58fba057ffb11534b1a299e
IvanIgnatov2 <149263221+IvanIgnatov2@users.noreply.github.com>
2023-10-28 21:09:10 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-class FildWord:
+import math
+import random
+import sys
+
+from PyQt5 import uic
+from PyQt5.QtWidgets import QMainWindow, QTableWidget, QListWidget, QLineEdit, QPushButton, QRadioButton
+from PyQt5.QtWidgets import QSpinBox, QLabel, QLCDNumber, QListView, QMainWindow, QApplication
+
+
+class FildWord(QMainWindow):
+    def __init__(self):
+        super(FildWord, self).__init__()
+        uic.loadUi('project 1.ui', self)
+
+        self.generatorButton.clicked.connect(self.generate)
+
+    def generate(self):
+        pass
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = WordTrick()
+    ex.show()
+    sys.exit(app.exec())
