b1eea390c2c9df1bacbe817a825c331674fe1b13
IvanIgnatov2 <149263221+IvanIgnatov2@users.noreply.github.com>
2023-11-08 00:35:17 +0300

    Add files via upload

--- project.py
+++ project.py
@@ -1,5 +1,7 @@
 import io
+import sqlite3
 import sys
+from random import randint
 
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow
@@ -191,7 +193,7 @@ menu = '''<?xml version="1.0" encoding="UTF-8"?>
  <connections/>
 </ui>
 '''
-list = '''<?xml version="1.0" encoding="UTF-8"?>
+records_list = '''<?xml version="1.0" encoding="UTF-8"?>
 <ui version="4.0">
  <class>MainWindow</class>
  <widget class="QMainWindow" name="MainWindow">
@@ -383,53 +385,135 @@ tabel = '''<?xml version="1.0" encoding="UTF-8"?>
 '''
 
 
-class Fildword_menu(QMainWindow):
+class Filword_menu(QMainWindow):
     def __init__(self):
         super().__init__()
         f = io.StringIO(menu)
         uic.loadUi(f, self)
         self.recordButton.clicked.connect(self.open_records)
         self.generatorButton.clicked.connect(self.generate)
-        self.radioButton.clicked.connect(self.choosing_a_theme)
-        self.radioButton_2.clicked.connect(self.choosing_a_theme)
-        self.radioButton_3.clicked.connect(self.choosing_a_theme)
-        self.radioButton_4.clicked.connect(self.choosing_a_theme)
+        self.radioButton.clicked.connect(self.choosing_a_theme_1)
+        self.radioButton_2.clicked.connect(self.choosing_a_theme_2)
+        self.radioButton_3.clicked.connect(self.choosing_a_theme_3)
+        self.radioButton_4.clicked.connect(self.choosing_a_theme_4)
+        self.Fildword_list_open = True
+        self.database_name = []
+        self.letters_for_table = []
+        self.list_of_tams_ids = []
+        self.words_for_filword = []
 
     def open_records(self):
-        self.Fildword_list_open = Fildword_list()
+        self.Fildword_list_open = Filword_list()
         self.Fildword_list_open.show()
 
     def generate(self):
-        self.Fildword_tabel_open = Fildword_tabel()
-        self.Fildword_tabel_open.show()
+        Filword_tabel.generate_2(self)
+        # Filword_game.start_time(self)
 
-    def choosing_a_theme(self):
-        pass
+    def choosing_a_theme_1(self):
+        self.database_name.append('database_1.db')
+
+    def choosing_a_theme_2(self):
+        self.database_name.append('database_2.db')
+
+    def choosing_a_theme_3(self):
+        self.database_name.append('database_3.db')
+
+    def choosing_a_theme_4(self):
+        self.database_name.append('database_4.db')
 
 
-class Fildword_tabel(QMainWindow):
+class Filword_tabel(QMainWindow):
     def __init__(self):
         super().__init__()
         f = io.StringIO(tabel)
         uic.loadUi(f, self)
+        self.letters_for_table = []
+        self.list_of_tams_ids = []
+        self.words_for_filword = []
+        self.Fildword_tabel_open = True
+
+    def generate_2(self):
+        for _ in range(10):
+            self.letters_for_table.append([])
+        for _ in range(self.spinBox.value()):
+            self.list_of_tams_ids.append(randint(0, len(Filword_menu.self.database_name)))
+        for list_ids in self.list_of_tams_ids:
+            conn = sqlite3.connect(f'{Filword_menu.self.database_name[list_ids]}')
+            cursor = conn.cursor()
+            cursor.execute('SELECT COUNT(*) FROM words')
+            row_count = cursor.fetchone()[0]
+            conn.close()
+            conn = sqlite3.connect('database_1.db')
+            cursor = conn.cursor()
+            id_for_table = randint(0, row_count)
+            cursor.execute(f"SELECT word FROM words WHERE id = {id_for_table}")
+            result = cursor.fetchone()
+            self.words_for_filword.append(result[0])
+            # Закрываем соединение с базой данных
+            conn.close()
+        counter = 0
+        for word in self.words_for_filword:
+            for letter in word:
+                self.letters_for_table[counter].append(letter)
+            counter += 1
+        self.Fildword_tabel_open = Filword_tabel()
+        self.Fildword_tabel_open.show()
 
 
-class Fildword_list(QMainWindow):
+class Filword_list(QMainWindow):
     def __init__(self):
         super().__init__()
-        f = io.StringIO(list)
+        f = io.StringIO(records_list)
         uic.loadUi(f, self)
 
+    def correction(self):
+        pass
 
-class Fildword_game(QMainWindow):
+
+class Filword_game(QMainWindow):
     def __init__(self):
         super().__init__()
         f = io.StringIO(game)
         uic.loadUi(f, self)
+        self.pushButton.clicked.connect(self.to_answer())
+        self.pushButton_3.clicked.connect(self.end)
+        self.Filword_tabel_open = True
+
+    def to_answer(self):
+        pass
+
+    def end(self):
+        pass
+
+    def start_time(self):
+        self.timer.start(1000)
+        self.Filword_tabel_open = Filword_game()
+        self.Filword_tabel_open.show()
+
+    def update_time(self):
+        time = self.label.text()
+        time_list = time.split(':')
+        hours, minutes, seconds = map(int, time_list)
+
+        seconds += 1
+
+        if seconds == 60:
+            seconds = 0
+            minutes += 1
+
+            if minutes == 60:
+                minutes = 0
+                hours += 1
+
+                if hours == 24:
+                    hours = 0
+
+        self.label.setText(f'{hours:02}:{minutes:02}:{seconds:02}')
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    Fildword_2 = Fildword_menu()
+    Fildword_2 = Filword_menu()
     Fildword_2.show()
     sys.exit(app.exec())
